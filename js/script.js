91300060096afcffdfce4fa5f3645e49cdee64ae
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-02-05 10:06:28 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const share1 = document.querySelector("#share1");
+const shareCo = document.querySelector(".share-component");
+const profileCo = document.querySelector(".profile-container");
+const share2 = document.querySelector("#share2");
+share1.addEventListener("click", () => {
+  if (window.innerWidth < 380) {
+    profileCo.classList.toggle("hidden");
+  }
+  shareCo.classList.toggle("hidden");
+});
+share2.addEventListener("click", () => {
+  if (window.innerWidth < 380) {
+    profileCo.classList.toggle("hidden");
+  }
+  shareCo.classList.toggle("hidden");
+});
