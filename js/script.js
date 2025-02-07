06f67653a33c44c0f2a95ca80823e7fff8d7db06
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-02-07 15:13:16 -0500

    Fixed issues: adjusted breakpoints, improved spacing, and ensured better mobile compatibility.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ const shareCo = document.querySelector(".share-component");
 const profileCo = document.querySelector(".profile-container");
 const share2 = document.querySelector("#share2");
 share1.addEventListener("click", () => {
-  if (window.innerWidth < 380) {
+  if (window.innerWidth < 1025) {
     profileCo.classList.toggle("hidden");
   }
   shareCo.classList.toggle("hidden");
 });
 share2.addEventListener("click", () => {
-  if (window.innerWidth < 380) {
+  if (window.innerWidth < 1025) {
     profileCo.classList.toggle("hidden");
   }
   shareCo.classList.toggle("hidden");
